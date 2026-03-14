e4c2567240e8383106018ee43b93b178d209564d
YWHo <YWHo@users.noreply.github.com>
2026-03-14 18:35:41 +1300

    feat: create LogoLink component

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -14,10 +14,24 @@ export interface SharedLink extends Struct.ComponentSchema {
   };
 }
 
+export interface SharedLogoLink extends Struct.ComponentSchema {
+  collectionName: 'components_shared_logo_links';
+  info: {
+    displayName: 'Logo Link';
+  };
+  attributes: {
+    href: Schema.Attribute.String;
+    image: Schema.Attribute.Media<'images'>;
+    isExternal: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
+    label: Schema.Attribute.String;
+  };
+}
+
 declare module '@strapi/strapi' {
   export module Public {
     export interface ComponentSchemas {
       'shared.link': SharedLink;
+      'shared.logo-link': SharedLogoLink;
     }
   }
 }
