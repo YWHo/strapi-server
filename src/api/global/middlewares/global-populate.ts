bdc69ae491cbed15548c1e21abbf550eac36d561
YWHo <YWHo@users.noreply.github.com>
2026-03-15 23:01:49 +1300

    feat: create middleware for Global API using Strapi generate

---
+++ src/api/global/middlewares/global-populate.ts
@@ -0,0 +1,14 @@
+/**
+ * `global-populate` middleware
+ */
+
+import type { Core } from '@strapi/strapi';
+
+export default (config, { strapi }: { strapi: Core.Strapi }) => {
+  // Add your own logic here.
+  return async (ctx, next) => {
+    strapi.log.info('In global-populate middleware.');
+
+    await next();
+  };
+};
